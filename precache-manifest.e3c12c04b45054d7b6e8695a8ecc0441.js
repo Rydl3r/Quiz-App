aecb2752118c6a09204167aa740d5d5d1b414e36
Ivan Mukoed <45137936+Rydl3r@users.noreply.github.com>
2020-06-26 13:15:27 +0300

    First Version of App

---
+++ precache-manifest.e3c12c04b45054d7b6e8695a8ecc0441.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aca20b3d4e0638ae5dfe140ee413b82",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5e1ff8f50fe6a43cef4c",
+    "url": "./static/css/main.b247649d.chunk.css"
+  },
+  {
+    "revision": "e9ddb870a08d9f31febb",
+    "url": "./static/js/2.5d9aecdd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.5d9aecdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e1ff8f50fe6a43cef4c",
+    "url": "./static/js/main.e1657cfa.chunk.js"
+  },
+  {
+    "revision": "f2323e27069c5284c651",
+    "url": "./static/js/runtime-main.11bf8585.js"
+  }
+]);
